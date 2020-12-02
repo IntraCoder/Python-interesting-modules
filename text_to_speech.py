4241334d0c63ce990b15b55d9732290345c993b3
IntraCoder <75381823+IntraCoder@users.noreply.github.com>
2020-12-02 21:33:07 +0530

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,11 @@
+import pyttsx3 as pt
+
+#Uncomment this to add properties
+#engine = pt.init()
+#voice=engine.getProperty('voices')
+#engine.setProperty("voices",voice[0])
+#engine.setProperty("rate",30)
+# engine.setProperty('voices', voice[1])
+
+pt.speak("Turned on! I'm Jarvis. Ready to assist you for your journey!")
+#
